d8cf8514fb99f874799ae1fbf2165865d3b8431f
Agustina Virasoro <agustinavirasoro@hotmail.com>
2021-01-25 13:21:37 -0300

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBarComponent from './components/navbar/';
+
+function App() {
+  return (
+    <>
+    <NavBarComponent />
+    Hola soy un proyecto
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './estilos.css'
+
+const NavBarComponent = () => {
+  return (
+    <>
+    <header>
+      <h1>Sweet Cakes</h1>
+      <nav>
+        <a href="">Home</a>
+        <a href="">Cakes</a>
+        <a href="">Pedidos</a>
+        <a href="">Contacto</a>
+      </nav>
+    </header> 
+    </>
+  );
+}
+
+export default NavBarComponent;
